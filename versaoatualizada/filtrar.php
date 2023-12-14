517343120b042420da0fce08118d4a74cb6744c8
ArtMonts <monteirotecnicoti@gmail.com>
2023-12-14 12:55:07 -0300

    FILTRO FUNCIONANDO

--- versaoatualizada/filtrar.php
+++ versaoatualizada/filtrar.php
@@ -1,5 +1,5 @@
 <?php 
-include'conexao.php';
+include ('conexao.php');
 include 'protect.php';
 if(isset($_POST['add_to_cart'])){
     $products_name = $_POST['product_name'];
@@ -25,7 +25,7 @@ if(isset($_POST['add_to_cart'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galeria</title>
+    <title>Filtro</title>
     <link rel="stylesheet" href="css/styleCarrinho">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -33,7 +33,14 @@ if(isset($_POST['add_to_cart'])){
 <?php 
 include('headerCarrinho.php');
 ?>
+
 <div class="container">
+    <!-- FILTRO -->
+  
+  <form action="">
+    <input type="text" placeholder="Digite a Roupa" value="<?php if(isset($_GET['busca'])) echo $_GET['busca'] ?>" name="busca">
+    <button type="submit">PESQUISAR</button>
+</form>
   
     <?php
 if(isset($display_message)){
@@ -46,82 +53,69 @@ if(isset($display_message)){
 }
 
 ?>
+
             <section class="products">
             <!-- CONTAINER ONDE FICAM TODOS OS PRODUTOS -->
 
             <!-- FILTROS -->
-<div class="container_filtro">
-  <form action="">
-<button type="submit" name="categoria" value="CALÇA" >Filtrar Pesquisa</button>
-</form>
-</div>
 
-            <div class="product_container">
+<?php 
+// se n tiver nada digitado, n vai aparecer nada 
+if(!isset($_GET['busca'])){
+    echo " ";
+}else{
+$pesquisa = $conn->real_escape_string($_GET['busca']);
+// pesquisando na tabela produtos e vendo se o que foi escrito se parece com oque tem no banco
+// a porcentagem significa que vai buscar qualquer coisa relacionada com o nome
+$sql_code = "SELECT * FROM products WHERE name LIKE '%$pesquisa%' OR type LIKE '%$pesquisa%'";
+$sql_query = $conn->query($sql_code);
 
-           
+if($sql_query->num_rows == 0){
+    echo "nenhum resultado encontrado...";
+}else{
 
+while($dados = $sql_query->fetch_assoc()){
 
-                <?php 
-                if(!isset($_GET['categoria'])){
-                  // $select_products = mysqli_query($conn, "select * from `products` ");
-       
-                // buscando dados da tabela dos produtos
-                $select_products = mysqli_query($conn, "select * from `products` ");
-               if(mysqli_num_rows($select_products) > 0){
-
-                  while( $fetch_product = mysqli_fetch_assoc($select_products)){
-                   // echo $fetch_product['name'];        
-                   // filtro
-                 ?> 
-
-                
-
+?>
+<!-- DADOS ESTÃO APARECENDO AQUI -->
 <form method="post" action="">
-                
+
 <li class="item">
          <div class="box">
            <div class="slide-img">
-             <img src="produtos/<?php echo $fetch_product['image']?>" alt="">
+             <img src="produtos/<?php echo $dados['image']?>" alt="">
              <div class="overlay">
                <a href="produtoespecifico.php" class="buy-btn">Compre agora</a>
              </div>
            </div>
            <div class="detail-box">
              <div class="type">
-             <a href="produtoespecifico.php"><?php echo $fetch_product['name'] ?></a>
-                    <div class="price"> <?php echo "R$ ".$fetch_product['price'] ?></div>
+             <a href="produtoespecifico.php"><?php echo $dados['name'] ?></a>
+                    <div class="price"> <?php echo "R$ ".$dados['price'] ?></div>
                </a>
                <!-- <span>Perfeita para usar no verão</span> -->
              </div>
              <br>
-             <!-- ICONE CARRINHO -->
              <ion-icon class="carrinho-icon" name="cart-outline"></ion-icon>           
            </div>
            <!-- CAMPOS OCULTOS QUE ESTÃO ARMAZENANDO OS DADOS PARA INSERIR NA TABELA CARRINHO PEGANDO DADOS DA TABELA PRODUTOS -->
-            <input type="hidden" name="product_name" value="<?php echo $fetch_product['name'] ?>">
-                    <input type="hidden" name="product_price"  value="<?php echo $fetch_product['price'] ?>">
-                    <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']?>">
-                    <input type="hidden" name="product_type" value="<?php echo $fetch_product['type']?>">
+            <input type="hidden" name="product_name" value="<?php echo $dados['name'] ?>">
+                    <input type="hidden" name="product_price"  value="<?php echo $dados['price'] ?>">
+                    <input type="hidden" name="product_image" value="<?php echo $dados['image']?>">
+                    <input type="hidden" name="product_type" value="<?php echo $dados['type']?>">
                     <!-- precisa aumentar o tamanho do botão -->
-                    <input type="submit"  class="submit_btn cart_btn" value="Adicionar ao Carrinho" name="add_to_cart" >
+                    <input type="submit"  class="submit_btn cart_btn" value="aaa ao Carrinho" name="add_to_cart" >
          </div>
        </li>
 </form>
 
+
 <?php
-          
-          // fim dos ifs e elses e while
-                  }
-                
-           }
-          }
-                else{
-                    // echo "<div class='empty_text'>Sem Produtos</div>";
-                    header('location: filtrar.php');
-                }
-                ?>
-                <!-- FORMULÁRIO -->
-                
+}
+}
+}
+?>
+                        <!-- FIM DO FILTRO -->             
             </div>
         </section>
     </div>
@@ -129,5 +123,6 @@ if(isset($display_message)){
     <!-- ionicon -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
 </body>
 </html>
